7390ebde7956206a7ad91d5add5b3c69cb42c9f8
Betsy E. Cabrera <bcabrera97@gmail.com>
2013-07-18 10:36:57 -0400

    form-based workflow feature completed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '85c5b5f567c744c347fcc5dc2f9b36a86a8d0a9d6aeffa646e37aff684bd6d533a970aa4be4a7ce3c323dbb94dcf7e5226a68b2c4056ccff7a969634460d0d10'
