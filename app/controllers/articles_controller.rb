e86bfbb7fceb0b7b5a13891494d7505e44a4275f
Betsy E. Cabrera <bcabrera97@gmail.com>
2013-07-19 12:00:19 -0400

    Tagging feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,9 +39,9 @@ class ArticlesController < ApplicationController
 	end
 
 
-	def article_params 
-		params.require("article").permit("title", "body")
-	end
+	def article_params
+    params.require(:article).permit(:title, :body, :tag_list)
+  end
 
 end
 
